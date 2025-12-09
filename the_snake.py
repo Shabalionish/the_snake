3e7aca1ec6381efa88887debe21ba5be8da6f7b1
Shabalionish <127481370+Shabalionish@users.noreply.github.com>
2025-12-09 23:38:17 +0500

    Update the_snake.py 6

--- the_snake.py
+++ the_snake.py
@@ -15,39 +15,31 @@ from typing import List, Optional, Tuple
 import pygame
 from pygame.locals import KEYDOWN, K_DOWN, K_LEFT, K_RIGHT, K_UP, QUIT
 
-# Константы для размеров поля и сетки
 SCREEN_WIDTH = 640
 SCREEN_HEIGHT = 480
 GRID_SIZE = 20
 GRID_WIDTH = SCREEN_WIDTH // GRID_SIZE
 GRID_HEIGHT = SCREEN_HEIGHT // GRID_SIZE
 
-# Направления движения
 UP = (0, -1)
 DOWN = (0, 1)
 LEFT = (-1, 0)
 RIGHT = (1, 0)
 
-# Цвета
 BOARD_BACKGROUND_COLOR = (0, 0, 0)
 BORDER_COLOR = (93, 216, 228)
 APPLE_COLOR = (255, 0, 0)
 SNAKE_COLOR = (0, 255, 0)
 
-# Скорость (FPS)
 SPEED = 10
 
-# Инициализация pygame
 pygame.init()
 
-# Глобальные переменные
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 clock = pygame.time.Clock()
 
 pygame.display.set_caption('Изгиб Питона')
 
-# Удобная константа для значения позиции по умолчанию, чтобы не делать
-# слишком длинные сигнатуры в объявлениях функций/классов.
 _DEFAULT_CENTER = (
     SCREEN_WIDTH // 2,
     SCREEN_HEIGHT // 2,
