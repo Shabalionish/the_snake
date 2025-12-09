409d47f47aa2355c303e110b38d0a28f803408b4
Shabalionish <127481370+Shabalionish@users.noreply.github.com>
2025-12-10 00:04:14 +0500

    Update the_snake.py final vers

--- the_snake.py
+++ the_snake.py
@@ -23,20 +23,25 @@ GRID_SIZE = 20
 GRID_WIDTH = SCREEN_WIDTH // GRID_SIZE
 GRID_HEIGHT = SCREEN_HEIGHT // GRID_SIZE
 
+# Направления движения
 UP = (0, -1)
 DOWN = (0, 1)
 LEFT = (-1, 0)
 RIGHT = (1, 0)
 
+# Цвета
 BOARD_BACKGROUND_COLOR = (0, 0, 0)
 BORDER_COLOR = (93, 216, 228)
 APPLE_COLOR = (255, 0, 0)
 SNAKE_COLOR = (0, 255, 0)
 
+# Скорость (FPS)
 SPEED = 10
 
+# Инициализация pygame
 pygame.init()
 
+# Глобальные переменные
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 clock = pygame.time.Clock()
 
